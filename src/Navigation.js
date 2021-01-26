0fb7d646228e9bd2d40f373d52ea234efde157fc
Claire Casey <clcasey805@gmail.com>
2021-01-26 12:32:52 -0800

    change class to className

--- src/Navigation.js
+++ src/Navigation.js
@@ -15,22 +15,22 @@ function Navigation() {
   return (
     <nav className="Nav navbar navbar-expand-lg navbar-light bg-light">
       <NavLink key="home" exact to="/" className="navbar-brand">Jobly</NavLink>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
             <NavLink key="companies" exact to="/companies" className="nav-link">Companies</NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <NavLink key="jobs" exact to="/jobs" className="nav-link">Jobs</NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <NavLink key="profile" exact to="/profile" className="nav-link">Profile</NavLink>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <button className="nav-link btn btn-link">Logout user</button>
           </li>
         </ul>
